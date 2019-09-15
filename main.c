486202ebcd1a8506e1343a320674b71679bc7271
Yupeng Li <yupengli@Yupengs-MacBook-Air.local>
2019-09-14 22:27:40 -0400

    modified main.c

--- main.c
+++ main.c
@@ -7,6 +7,7 @@
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 
 float blinkfrequency=0.5;//set the blink frequency to be 50Hz
+float dutycyle = 0.5
 int main(void)
 {
     teensy_clockdivide(0); //set the clock speed
@@ -16,7 +17,10 @@ int main(void)
     for(;;){
         /* insert your main loop code here */
         teensy_led(TOGGLE);	// switch the led state
-        teensy_wait(1/blinkfrequency/2*1000);		// wait for the blink interval
+        teensy_wait(1/blinkfrequency/2*1000*(1-dutycyle));		// wait for the blink interval
+        tennsy_led(TOGGLE);
+        tennsy_wait(1/blinkfrequency/2*1000*dutycycle);
+
     }
     return 0;   /* never reached */
 }
