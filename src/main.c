a7f1375156205f59907847205fc363011441e83f
Yupeng Li <yupengli@Yupengs-MacBook-Air.local>
2019-09-18 00:26:24 -0400

    Add source code

---
+++ src/main.c
@@ -0,0 +1,26 @@
+/* Name: main.c
+ * Author: <insert your name here>
+ * Copyright: <insert your copyright message here>
+ * License: <insert your license reference here>
+ */
+
+#include "teensy_general.h"  // includes the resources included in the teensy_general.h file
+
+int main(void)
+{
+    m_usb_init();
+    clear(DDRC,6);
+    set(DDRD,2);
+    while(1)
+    if (bit_is_set(PORTC,6))
+    {
+      clear(PORTD,2);
+    }
+    else if (!bit_is_set(PORTC,6))
+    {
+      set(PORTD,2);
+    }
+
+    }
+    return 0;   /* never reached */
+}
