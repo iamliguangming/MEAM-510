8a44726d564886b7f4d00ff4217a7e1f379933bc
Yupeng Li <yupengli@huntsman-ve703-01848.apn.wlan.private.upenn.edu>
2019-09-19 17:13:41 -0400

    PRINT

--- src/main.c
+++ src/main.c
@@ -6,20 +6,27 @@
 
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 #include "t_usb.h"
+#define PRINTNUM(x) m_usb_tx_uint(x); m_usb_tx_char(10);m_usb_tx_char(13);
+
 int main(void)
 {
     m_usb_init();
     clear(DDRC,6);
     set(DDRD,2);
+    set(TCCR3B,CS30);
+    set(TCCR3B,CS32);
     for(;;)
     {
     if (bit_is_set(PINC,6))
     {
       clear(PORTD,2);
+      PRINTNUM(TCNT3);
+
     }
     else if (!bit_is_set(PINC,6))
     {
       set(PORTD,2);
+      PRINTNUM(TCNT3);
     }
 }
 
