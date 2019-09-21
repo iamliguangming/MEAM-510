edd4a674ac0a630e8415bb704277bb7240b60d3c
Yupeng Li <yupengli@seas.upenn.edu>
2019-09-21 03:30:08 -0400

    Code for 2.1.1 and 2.1.2 without comment

---
+++ past code/2.1.1.c
@@ -0,0 +1,39 @@
+/* Name: main.c
+ * Author: <insert your name here>
+ * Copyright: <insert your copyright message here>
+ * License: <insert your license reference here>
+ */
+
+#include "teensy_general.h"  // includes the resources included in the teensy_general.h file
+#include "t_usb.h"
+#define PRINTNUM(x) m_usb_tx_uint(x); m_usb_tx_char(10);m_usb_tx_char(13);
+
+int main(void)
+{
+    m_usb_init();
+    clear(DDRC,6);
+    set(DDRD,2);
+    set(TCCR3B,CS30);
+    set(TCCR3B,CS32);
+
+    for(;;)
+    {
+    if (bit_is_set(PINC,6))
+    {
+      clear(PORTD,2);
+      PRINTNUM(TCNT3);
+      m_usb_tx_string("Now it's off");
+      while(bit_is_set(PINC,6));
+
+    }
+    else if (!bit_is_set(PINC,6))
+    {
+      set(PORTD,2);
+      PRINTNUM(TCNT3);
+      m_usb_tx_string("Now it's on");
+      while(!bit_is_set(PINC,6));
+    }
+}
+
+    return 0;   /* never reached */
+}
