edcbfabf3bc1951e2a3bfff77d344526d3a9ddb1
Yupeng Li <yupengli@seas.upenn.edu>
2019-09-26 15:26:51 -0400

    LAB 2 part2

--- src/main.c
+++ src/main.c
@@ -6,28 +6,42 @@
 
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 #include "t_usb.h"
+#define PRINTNUM(x) m_usb_tx_uint(x); m_usb_tx_char(10);m_usb_tx_char(13); //Print number x and start off a new line
 
+unsigned int oldtime, tperiod;
+void waitfortrigger();
 int main(void)
 {
     m_usb_init();
     set(DDRD,2);
+    set(DDRD,1);
     set(TCCR3B,CS30);
     set(TCCR3B,CS32);
 
     for(;;)
     {
-    if (bit_is_set(PINC,7))
-    {
-      set(PORTD,2);
-      while(bit_is_set(PINC,7));
+      waitfortrigger();
+      if(tperiod >= 52)
+      {
+        set(PORTD,2);
+        clear(PORTD,1);
+        PRINTNUM(tperiod);
+      }
+      else if (tperiod < 52)
+      {
+        set(PORTD,1);
+        clear(PORTD,2);
+        PRINTNUM(tperiod);
+      }
 
     }
-    else if (!bit_is_set(PINC,7))
-    {
-      clear(PORTD,2);
-      while(!bit_is_set(PINC,7));
-    }
-}
 
     return 0;   /* never reached */
+}
+void waitfortrigger()
+{
+  while(!bit_is_set(TIFR3,ICF3));
+  set(TIFR3,ICF3);
+  tperiod = ICR3 - oldtime;
+  oldtime = ICR3;
 }
