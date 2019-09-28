c663f535f5aea7fee89e7e5d4fd1ea218754ee8d
Yupeng Li <yupengli@seas.upenn.edu>
2019-09-28 04:22:58 -0400

    Updated code for main.c and main.hex. Hope it would work

--- src/main.c
+++ src/main.c
@@ -7,8 +7,12 @@
 #include "teensy_general.h"  // includes the resources included in the teensy_general.h file
 #include "t_usb.h"
 #define PRINTNUM(x) m_usb_tx_uint(x); m_usb_tx_char(10);m_usb_tx_char(13); //Print number x and start off a new line
+#define PRINTSTRING(x) m_usb_tx_string(x); m_usb_tx_char(10); m_usb_tx_char(13);
 
+int xflag = 0;
+int syncflag = 0;
 unsigned int startofpulse, endofpulse, pulsewidth;
+unsigned int lastY, lastX, lastsync;
 int main(void)
 {
     m_usb_init();
@@ -26,10 +30,38 @@ int main(void)
       pulsewidth = endofpulse - startofpulse;
       set(TCCR3B, ICF3);
       toggle(TCCR3B,ICES3);
-      if (pulsewidth < something)
+      if (pulsewidth > 13 && pulsewidth < 25)
       {
-        
+        if (syncflag == 1)
+        {
+          xflag = 1;
+        }
+        else if (syncflag ==0)
+        {
+          syncflag = 1;
+        }
+        lastsync = startofpulse;
       }
+      if (pulsewidth > 3 && pulsewidth < 6)
+      {
+          if (xflag == 0)
+          {
+            lastY = startofpulse;
+            PRINTSTRING("The time between last Y pulse and Sync pulse is:");
+            PRINTNUM(lastY-lastsync);
+            syncflag = 0;
+          }
+          if (xflag != 0)
+          {
+            lastX = startofpulse;
+            PRINTSTRING("The time between last X pulse and Sync pulse is:");
+            PRINTNUM(lastX-lastsync);
+            syncflag = 0;
+            xflag = 0;
+
+          }
+      }
+
 
 
 
