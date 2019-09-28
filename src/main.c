f042a09d07846f39ad76af5771e1b82cde3d7e1c
Yupeng Li <yupengli@seas.upenn.edu>
2019-09-27 22:37:52 -0400

    with wired bug

--- src/main.c
+++ src/main.c
@@ -8,22 +8,27 @@
 #include "t_usb.h"
 #define PRINTNUM(x) m_usb_tx_uint(x); m_usb_tx_char(10);m_usb_tx_char(13); //Print number x and start off a new line
 
+int syncflag = 0;
+int noxyflag = 0;
 unsigned int oldtime, tperiod, startofpulse, endofpulse;
-unsigned int pulsewidth;
+unsigned int pulsewidth, lastsynctime,lastxtime,lastytime;
+unsigned int xwithsync, ywithsync;
 void waitfortrigger();
 int main(void)
 {
     m_usb_init();
     set(TCCR3B,CS30);
     set(TCCR3B,CS31);
-    set(TCCR3B,ICES3)
+    set(TCCR3B,ICES3);
     for(;;)
     {
       waitfortrigger();
+      PRINTNUM(1);
     }
 
     return 0;   /* never reached */
 }
+
 void waitfortrigger()
 {
   while(!bit_is_set(TIFR3,ICF3));
@@ -43,8 +48,44 @@ void waitfortrigger()
   if (startofpulse != 0 && endofpulse !=0)
     {
       pulsewidth = endofpulse - startofpulse;
-      startofpulse = 0;
-      endofpulse = 0;
     }
+
   }
+  if (pulsewidth > 3 && pulsewidth <6)//for x,y pulse
+  {
+    if (syncflag == 0)
+    {
+      syncflag = 1;
+    }
+    if (syncflag ==1 )
+    {
+      noxyflag = 1;
+    }
+  lastsynctime = endofpulse;
+
+  }
+  if (pulsewidth > 12.5 && pulsewidth < 25)//for sync pulse
+  {
+   if (noxyflag == 1)
+   {
+     syncflag = 0;
+     noxyflag = 0;
+     lastxtime = endofpulse;
+   }
+   if (noxyflag == 0)
+   {
+     syncflag = 0;
+     lastytime = endofpulse;
+   }
+  }
+  xwithsync = lastxtime - lastsynctime;
+  m_usb_tx_string("Time between x and sync");
+  m_usb_tx_char(10);m_usb_tx_char(13);
+  PRINTNUM(xwithsync);
+  ywithsync = lastytime - lastsynctime;
+  m_usb_tx_string("Time between y and sync");
+  m_usb_tx_char(10);m_usb_tx_char(13);
+  PRINTNUM(ywithsync);
+  toggle(TCCR3B,ICES3);
+
 }
