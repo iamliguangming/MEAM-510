0b6013e159923bd562f28ed696b4930861d64cae
Yupeng Li <yupengli@seas.upenn.edu>
2019-09-28 22:52:30 -0400

    Code that works at around 1 m

--- src/main.c
+++ src/main.c
@@ -13,59 +13,70 @@ int xflag = 0;
 int syncflag = 0;
 unsigned int startofpulse, endofpulse, pulsewidth;
 unsigned int lastY, lastX, lastsync;
+
 int main(void)
 {
     m_usb_init();
-    set(TCCR3B,CS30);
-    set(TCCR3B,CS31);
     set(TCCR3B,ICES3);
+    set(TCCR3B,CS31);
+    set(TCCR3B,CS30);
+
+    teensy_wait(200);
+    PRINTSTRING("Please wait for started");
+
     for(;;)
     {
-      while (!bit_is_set(TCCR3B,ICF3));
+
+      while (!bit_is_set(TIFR3,ICF3));
       startofpulse = ICR3;
-      set(TCCR3B,ICF3);
+      set(TIFR3,ICF3);
       toggle(TCCR3B,ICES3);
-      while (!bit_is_set(TCCR3B,ICF3));
+      while (!bit_is_set(TIFR3,ICF3));
       endofpulse = ICR3;
       pulsewidth = endofpulse - startofpulse;
-      set(TCCR3B, ICF3);
+      set(TIFR3, ICF3);
       toggle(TCCR3B,ICES3);
-      if (pulsewidth > 13 && pulsewidth < 25)
+      if (pulsewidth >= 12 && pulsewidth < 68)
       {
-        if (syncflag == 1)
-        {
-          xflag = 1;
-        }
-        else if (syncflag ==0)
+        // PRINTNUM(1);
+        if (syncflag == 0)
         {
           syncflag = 1;
         }
-        lastsync = startofpulse;
+        else if (syncflag ==1)
+        {
+          xflag = 1;
+          syncflag = 0;
+        }
+      lastsync = startofpulse;
       }
-      if (pulsewidth > 3 && pulsewidth < 6)
+
+
+      if (pulsewidth > 2 && pulsewidth < 6)
       {
+         // PRINTNUM(0);
           if (xflag == 0)
           {
-            lastY = startofpulse;
-            PRINTSTRING("The time between last Y pulse and Sync pulse is:");
-            PRINTNUM(lastY-lastsync);
+            lastY = startofpulse - lastsync;
+
             syncflag = 0;
           }
           if (xflag != 0)
           {
-            lastX = startofpulse;
-            PRINTSTRING("The time between last X pulse and Sync pulse is:");
-            PRINTNUM(lastX-lastsync);
+            lastX = startofpulse - lastsync;
+
             syncflag = 0;
             xflag = 0;
 
           }
       }
-
-
-
-
+      PRINTSTRING("The time between last X Y pulse and Sync pulse is:");
+      m_usb_tx_uint(lastX);
+      m_usb_tx_char(3);
+      PRINTNUM(lastY);
     }
+
+
 
     return 0;   /* never reached */
 }
